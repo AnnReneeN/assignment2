d72a4377593b27d8ddcd6e9a44c9948e85fad6ff
Annrenee Nachwa <P15214723@LEC-ADMIN.dmu.ac.uk>
2017-12-13 22:02:07 +0000

    sorted out the media queries

--- javascript.js
+++ javascript.js
@@ -1,11 +1,10 @@
-$("#slideshow > div:gt(0)").hide();
+jQuery("#slideshow > div:gt(0)").hide();
 
 setInterval(function() {
-  $('#slideshow > div:first')
+  jQuery('#slideshow > div:first')
     .fadeOut(1000)
     .next()
     .fadeIn(1000)
     .end()
     .appendTo('#slideshow');
-}, 3000);
-
+}, 3000)
